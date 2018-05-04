7d6ffea776dcefc217c967576a74ec2904fffc4e
Tobias <tobias.f.wolf@me.com>
2018-05-04 14:58:07 +0200

    init

---
+++ src/hyphenate.js
@@ -0,0 +1,72 @@
+import Hypher from 'hypher';
+import { UI, Settings } from 'sketch';
+import availableLanguages from './languages';
+
+const languageOptions = availableLanguages.map(language => language.id);
+
+function choseLanguageDialog(context, title = 'Language Pattern') {
+  const selection = UI.getSelectionFromUser(
+    title,
+    languageOptions
+  );
+  const value = languageOptions[selection[1]];
+  Settings.setDocumentSettingForKey(context.document.documentData(), 'chosen-language', value)
+}
+
+function getSelectedTextLayers(selectedLayers) {
+  const textLayers = [];
+  if (selectedLayers) {
+    selectedLayers.forEach(layer => {
+      if (layer.stringValue && layer.setStringValue) {
+        textLayers.push(layer);
+      }
+    });
+  }
+  return textLayers;
+}
+
+export function addHyphens(context) {
+  if (!Settings.documentSettingForKey(context.document.documentData(), 'chosen-language')) {
+    choseLanguageDialog(context);
+  }
+
+  const chosenLanguage = Settings.documentSettingForKey(context.document.documentData(), 'chosen-language');
+  // const chosenLanguage = 'de';
+  const language = availableLanguages.find(_language => _language.id === chosenLanguage);
+
+  const hypher = new Hypher(language);
+  const textLayers = getSelectedTextLayers(context.selection);
+  textLayers.forEach(layer => {
+    const hypenated = hypher.hyphenateText(layer.stringValue());
+    layer.setStringValue(hypenated);
+  });
+  if (textLayers.length === 0) {
+    UI.message('Please chose text layers to add hyphens.');
+  } else if (textLayers.length === 1) {
+    UI.message(`Hyphenated ${textLayers.length} layer (Chosen Language: ${language.id}).`);
+  } else {
+    UI.message(`Hyphenated ${textLayers.length} layers (Chosen Language: ${language.id}).`);
+  }
+}
+
+
+export function removeHyphens(context) {
+  const textLayers = getSelectedTextLayers(context.selection);
+
+  textLayers.forEach(layer => {
+    let text = layer.stringValue();
+    text = text.replace(/[\u00AD]/g, '');
+    layer.setStringValue(text);
+  });
+  if (textLayers.length === 0) {
+    UI.message('Please chose text layers to remove hyphens.');
+  } else {
+    UI.message(`Removed hyphens from ${textLayers.length} layers`);
+  }
+
+}
+
+
+export function changeLanguage(context) {
+  choseLanguageDialog(context, 'Change Language Pattern');
+}
---
+++ src/languages.js
@@ -0,0 +1,81 @@
+import be from './patterns/be';
+import bn from './patterns/bn';
+import ca from './patterns/ca';
+import cs from './patterns/cs';
+import da from './patterns/da';
+import de from './patterns/de';
+import elMonoton from './patterns/el-monoton';
+import elPolyton from './patterns/el-polyton';
+import enGb from './patterns/en-gb';
+import enUs from './patterns/en-us';
+import es from './patterns/es';
+import fi from './patterns/fi';
+import fr from './patterns/fr';
+import grc from './patterns/grc';
+import gu from './patterns/gu';
+import hi from './patterns/hi';
+import hu from './patterns/hu';
+import hy from './patterns/hy';
+import is from './patterns/is';
+import it from './patterns/it';
+import kn from './patterns/kn';
+import la from './patterns/la';
+import lt from './patterns/lt';
+import lv from './patterns/lv';
+import ml from './patterns/ml';
+import nbNo from './patterns/nb-no';
+import nl from './patterns/nl';
+import or from './patterns/or';
+import pa from './patterns/pa';
+import pl from './patterns/pl';
+import pt from './patterns/pt';
+import ru from './patterns/ru';
+import sk from './patterns/sk';
+import sl from './patterns/sl';
+import sv from './patterns/sv';
+import ta from './patterns/ta';
+import te from './patterns/te';
+import tr from './patterns/tr';
+import uk from './patterns/uk';
+
+export default [
+  be,
+  bn,
+  ca,
+  cs,
+  da,
+  de,
+  elMonoton,
+  elPolyton,
+  enGb,
+  enUs,
+  es,
+  fi,
+  fr,
+  grc,
+  gu,
+  hi,
+  hu,
+  hy,
+  is,
+  it,
+  kn,
+  la,
+  lt,
+  lv,
+  ml,
+  nbNo,
+  nl,
+  or,
+  pa,
+  pl,
+  pt,
+  ru,
+  sk,
+  sl,
+  sv,
+  ta,
+  te,
+  tr,
+  uk,
+];
